10101eab02038cc0c633e5681305497437a96b61
BasPS <example@email.com>
2021-09-12 16:32:46 +0700

    complete randomMessages project

--- RandomMessages.js
+++ RandomMessages.js
@@ -1,1 +1,32 @@
-console.log('hello node JS');
+// add any item in object
+const item = {
+    gems: ['diamond','pearl','ruby','sapphire','emerald','alexandrite','oriental catseye'] ,
+    accessories: ['ring','pendant','anklet','bracelet','earring','brooch','locket'],
+}; 
+// random number 0 --> num
+const randomIndex = num => Math.floor(Math.random() * num);
+// array that gather text
+let text = [];
+// random item and sent text to array
+for(let keys in item) {
+    let idex = randomIndex(item[keys].length);
+    switch (keys) {
+        case 'gems':
+            text.push('Your lucky gemstone is ' + item[keys][idex] + '.');
+            break;
+        case 'accessories':
+            text.push('We offer ' + item[keys][idex] + ' for you.');           
+            break;
+    
+        default:
+            text.push('Item not found');
+            break;
+    }
+};
+// formatted array and print to console
+const formattedText = text => {
+    let newText = text.join('\n');
+    console.log(newText);
+}
+
+formattedText(text);
